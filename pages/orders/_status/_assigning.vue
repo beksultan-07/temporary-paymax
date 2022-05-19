9add564e8bc932d4b5ea6b54c4ed85bb272d7574
paymex <paymex.center@gmail.com>
2022-05-19 18:39:55 +0300

    👷 Daily update

--- pages/orders/_status/_assigning.vue
+++ pages/orders/_status/_assigning.vue
@@ -140,7 +140,7 @@
     </template>
 
     <v-overlay absolute :color="$vuetify.theme.dark ? 'grey darken-4' : 'white'" opacity="0.8" :value="overlay">
-      <v-progress-circular color="primary" indeterminate size="50" />
+      <v-progress-circular color="yellow darken-3" indeterminate size="50" />
     </v-overlay>
 
   </div>
