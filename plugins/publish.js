13c18a2bb4a6d7e7bf246a3931ce6f465d0bf920
paymex <paymex.center@gmail.com>
2022-07-02 18:01:56 +0300

    🏮 Daily update

--- plugins/publish.js
+++ plugins/publish.js
@@ -1,14 +1,21 @@
 import mqtt from 'mqtt';
 import Vue from 'vue';
 
+/**
+ * @param app
+ * @param inject
+ * @returns {boolean}
+ */
 export default ({ app }, inject) => {
   let event = new Vue();
+
   app.$publish = {
-    route: null,
     client: mqtt.connect(process.env.BASE_BROKER || 'ws://localhost:15675/ws', {
       clean: true
     }),
     subscribe(topic, channels, error) {
+
+      // Connect broker client.
       this.client.on('connect', () => {
         this.client.subscribe(topic, {
           qos: 0
@@ -16,31 +23,40 @@ export default ({ app }, inject) => {
           if(err) {
             error(`Error on topic subscribe: ${err}`);
           }
-          this.client.on("message", (t, m, packet) => {
-            //if (!packet.qos || !m.byteLength) {
-            //  return;
-            //}
-            if (topic === t) {
-              let bytea = JSON.parse(m.toString());
-              channels.map((channel) => {
-                if (channel === bytea.channel) {
-                  event.$emit(channel, JSON.parse(bytea.data));
-                }
-              });
+          console.log('Connect to a trading broker.')
+        });
+      });
+
+      // Reconnect broker client.
+      this.client.on("reconnect", () => {
+        console.log('Reconnecting to a trading broker.');
+      });
+
+      // Message broker client.
+      this.client.on("message", (t, m, packet) => {
+        if (!m.byteLength) {
+          return;
+        }
+        if (topic === t) {
+          let bytea = JSON.parse(m.toString());
+          channels.map((channel) => {
+            if (channel === bytea.channel) {
+              event.$emit(channel, JSON.parse(bytea.data));
             }
           });
-        });
+        }
       });
     },
     unbind(channels) {
       channels.map((channel) => {
-        event.$off(channel);
+        event.$off(channel)
       });
     },
     bind(channel, callback) {
       if (typeof callback !== "function") {
         return false
       }
+
       event.$on(channel, callback);
     }
   };
