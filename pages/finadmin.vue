1aa1715d31f2ff33bb118fd139d2a394f00a0f41
Beksultan <bakytuulubeksultan@gmail.com>
2022-09-27 12:51:37 +0600

    init fin admin page

---
+++ pages/finadmin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hello">
+        hello project
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .hello {
+        color: red;
+    }
+</style>
