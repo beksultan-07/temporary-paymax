d0447d33be1f0f99a31a64a59ea879c080e04482
HydroOx1d <n-esenov44@mail.ru>
2022-10-10 14:14:29 +0600

    base layout of database

---
+++ pages/database/clients.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container style="max-width: 1640px;">
+    <v-card outlined>
+      <v-row no-gutters>
+        <v-col cols="3">
+          SideBar
+        </v-col>
+        <v-col cols="9">
+          Main
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    auth: false
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
