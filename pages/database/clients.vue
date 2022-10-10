d211191f46cbabd92a6464b459b4ad39d2fb3e97
Beksultan <bakytuulubeksultan@gmail.com>
2022-10-10 17:24:37 +0600

    fix

--- pages/database/clients.vue
+++ pages/database/clients.vue
@@ -2,20 +2,29 @@
   <v-container style="max-width: 1640px;">
     <v-card outlined>
       <v-row no-gutters>
+
         <v-col cols="3">
           SideBar
         </v-col>
+
         <v-col cols="9">
-          Main
+          <v-database-table/>
         </v-col>
+      
       </v-row>
     </v-card>
   </v-container>
 </template>
 
 <script>
+  import DatabaseTableVue from '../../components/Database/DatabaseTable/DatabaseTable.vue';
+
   export default {
-    auth: false
+    auth: false,
+
+    components:{
+      'v-database-table': DatabaseTableVue
+    }
   }
 </script>
 
