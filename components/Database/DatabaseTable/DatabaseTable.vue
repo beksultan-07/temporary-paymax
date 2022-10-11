f6bf5f80659911187fa7131b773b80438b1f2c74
Beksultan <bakytuulubeksultan@gmail.com>
2022-10-11 09:42:10 +0600

    modified input database table

--- components/Database/DatabaseTable/DatabaseTable.vue
+++ components/Database/DatabaseTable/DatabaseTable.vue
@@ -1,30 +1,31 @@
 <template>
-    <v-tabs>
-        <v-tab href="tab-individuals" @click.prevent>
-            Физические лица
-        </v-tab>
-        <v-tab href="tab-legalEntities" @click.prevent>
-            Юридические лица
-        </v-tab>
-
-        <v-text-field
-            solo
-            label="Поиск"
-            append-icon="mdi-magnify"
-            :full-width="false"
-            flat
-            class="input"
-          ></v-text-field>
-
-        <v-tab-item value="tab-individuals">
-            <v-container>Физические лица</v-container>
-        </v-tab-item>
-
-        <v-tab-item value="tab-legalEntities">
-            <v-container>Юридические лица</v-container>
-        </v-tab-item>
-
-  </v-tabs>
+        <v-tabs >
+            <v-tab href="tab-individuals" @click.prevent>
+                Физические лица
+            </v-tab>
+            <v-tab href="tab-legalEntities" @click.prevent>
+                Юридические лица
+            </v-tab>
+    
+            <v-text-field
+                solo
+                label="Поиск"
+                append-icon="mdi-magnify"
+                :full-width="false"
+                :flat="$vuetify.theme.dark ? false : true"
+                :background-color="$vuetify.theme.dark ? '#1e1e1e' : '#f9f9f9'" 
+                class="input"
+              ></v-text-field>
+    
+            <v-tab-item value="tab-individuals">
+                <v-container>Физические лица</v-container>
+            </v-tab-item>
+    
+            <v-tab-item value="tab-legalEntities">
+                <v-container>Юридические лица</v-container>
+            </v-tab-item>
+            
+        </v-tabs>
 </template>
 
 <script>
